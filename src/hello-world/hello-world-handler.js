aa00c1ac542c1e388ab401d300259308d7830f69
vitor-tadashi <vitor.tadashi@outlook.com>
2018-07-24 08:27:43 -0300

    feature(helloWorld)

---
+++ src/hello-world/hello-world-handler.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports.hello = (event, context, callback) => {
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless! Hello World!',
+      input: event,
+    }),
+  };
+
+  callback(null, response);
+};
